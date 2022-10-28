92534ead62dc2cc7de58cd04e4d4b106bfc8312c
Juan Urdaneta <67289885+Juandurdaneta@users.noreply.github.com>
2022-10-28 11:39:28 -0400

    Added error handling

--- calc/routes.js
+++ calc/routes.js
@@ -10,37 +10,51 @@ try{
     const players = req.body.players;
     const board = req.body.board;
 
-    const Table = new odds_calc.TexasHoldem();
+    if(!players){
+        res.send({
+            status: 403,
+            message: 'No players were added, check your input and try again.'
+        })
+    } else{
 
-    players.forEach(player => {
-        let cards = player.split(/(..)/g).filter(s => s);
-        console.log(cards)
-        Table.addPlayer(cards)
-    });
+        const Table = new odds_calc.TexasHoldem();
 
-    const table_cards = board.split(/(..)/g).filter(s => s);
-    Table.setBoard(table_cards)
+        players.forEach(player => {
+            let cards = player.split(/(..)/g).filter(s => s);
+            console.log(cards)
+            Table.addPlayer(cards)
+        });
 
-    console.log(table_cards)
+        // In case the user doesn't add a board to the body of the request
 
-    const Result = Table.calculate();
+        if(board){ 
+            const table_cards = board.split(/(..)/g).filter(s => s);
+            Table.setBoard(table_cards)
+        }
 
-    const results = []
-    Result.getPlayers().forEach(player => {
-        results.push(`${player.getName()} - ${player.getHand()} - Wins: ${player.getWinsPercentageString()} - Ties: ${player.getTiesPercentageString()}`);
-      });
-      
 
-    res.send({
-        status: 200,
-        results: results
-    });
+        const Result = Table.calculate();
+
+        const results = []
+        Result.getPlayers().forEach(player => {
+            results.push(`${player.getName()} - ${player.getHand()} - Wins: ${player.getWinsPercentageString()} - Ties: ${player.getTiesPercentageString()}`);
+        });
+        
+
+        res.send({
+            status: 200,
+            results: results
+        });
+
+    }
+    
 
 } catch(err){
     console.log(err)
     res.send({
         status: 403,
-        message: 'Something went wrong, check your input and try again...'
+        message: 'Something went wrong, check your input and try again...',
+        error_log: `${err.stack}`
     })
 }
 
